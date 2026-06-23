201e241ad4ee9aa63963cb75489b8e47cc49496a
EUNCHEOK <kjh82701@naver.com>
2026-06-23 18:14:26 +0900

    Revert "WIP: local changes"
    This reverts commit 4de687c500e28d99565137997eab9b11f0f55b24.

--- Backend/bookapp/src/main/java/com/team20/bookapp/config/WebConfig.java
+++ Backend/bookapp/src/main/java/com/team20/bookapp/config/WebConfig.java
@@ -1,3 +1,8 @@
+/*
+   Webconfig.java
+   - CORS 설정
+ */
+
 package com.team20.bookapp.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -7,12 +12,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://3.144.149.46")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")      // 모든 경로(e.g., `/api/books`)에 이 CORS 규칙을 적용
+                .allowedOrigins("http://localhost:5173")       // 요청을 허용할 '방문자 명단’에React 앱 주소(`:5173`)를 등록
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")     //React 앱이 서버에 요청할 수 있는 method 허용
+                .allowedHeaders("*")        // 모든 HTTP 헤더 포함 허용
+                .allowCredentials(true)     // (옵션) 로그인 구현 시 인증 전보 전달 허용
                 .maxAge(3600);
     }
 }
