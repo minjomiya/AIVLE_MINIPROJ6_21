68761aff8adff6687e78c3f5c7f75e19cb168af2
EUNCHEOK <kjh82701@naver.com>
2026-06-23 18:14:36 +0900

    Revert "deploy"
    This reverts commit f4f306357629f524f819c723ef913772bcc0aca2.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,11 +9,11 @@ cd "$APP_DIR"
 
 pkill -f 'java -jar' || true
 
-JAR_FILE=$(ls Backend/bookapp/build/libs/*.jar | grep -v plain | head -n 1)
-nohup java -jar "$JAR_FILE" > backend.log 2>&1 &
+nohup java -jar Backend/bookapp/build/libs/*.jar > backend.log 2>&1 &
 
 rm -rf /usr/share/nginx/html/*
 cp -r Frontend/dist/* /usr/share/nginx/html/
 
 systemctl enable nginx
 systemctl restart nginx
+
